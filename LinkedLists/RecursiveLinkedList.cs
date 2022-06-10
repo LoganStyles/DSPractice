ce8f55dc350fbee97e38debdd219e2a2b394d61b
LoganStyles <emmaist23@gmail.com>
2022-06-10 12:47:49 +0100

    Initial commit

---
+++ LinkedLists/RecursiveLinkedList.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPractice.LinkedLists
+{
+    internal class RecursiveLinkedList
+    {
+        private class Node
+        {
+
+            public Node(int val)
+            {
+                Val = val;
+            }
+
+            public int Val { get; }
+            public Node Next { get; set; }
+        }
+
+        private Node Head;
+
+        //Time: O(n)
+        //Space: O(n)
+        public void Append(int input)
+        {
+            if (Head == null)
+            {
+                Head = new Node(input);
+                return;
+            }
+
+            _Append(input, Head);
+        }
+
+        private void _Append(int input, Node current)
+        {
+            if (current.Next == null)
+            {
+
+                current.Next = new Node(input);
+                return;
+            }
+
+            _Append(input, current.Next);
+        }
+
+        //Time: O(n)
+        //Space: O(n)
+        public bool Contains(int target)
+        {
+            return _Contains(Head, target);
+        }
+
+        private bool _Contains(Node current, int target)
+        {
+            if (current == null)
+                return false;
+
+            if (current.Val == target)
+                return true;
+
+            return _Contains(current.Next, target);
+        }
+
+        //Time: O(n)
+        //Space: O(n)
+        public int Sum()
+        {
+            //return _Sum(Head, 0);
+            return _Sum2(Head);//alternative
+        }
+
+        private int _Sum(Node current, int sum)
+        {
+            if (current == null)
+                return sum;
+
+            return _Sum(current.Next, sum += current.Val);
+        }
+
+        private int _Sum2(Node current)
+        {
+            if (current == null)
+                return 0;
+
+            return current.Val + _Sum2(current.Next);
+        }
+
+        //Time: O(n)
+        //Space: O(n)
+        public void Print()
+        {
+            _Print(Head, 1);
+        }
+
+        private void _Print(Node current, int index)
+        {
+            if (current == null)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            Console.WriteLine($"Node value {index}: {current.Val}");
+            _Print(current.Next, index + 1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DSPractice.LinkedLists;
+
+Console.WriteLine("Hello, Data structures!");
+
+#region LinkedLists
+
+#region Iterative LinkedList
+//var myLinkedList = new IterativeLinkedList();
+//myLinkedList.Append(1);
+//myLinkedList.Append(2);
+//myLinkedList.Append(3);
+//myLinkedList.Append(4);
+
+//myLinkedList.Print();
+
+//var status = false;
+//status = myLinkedList.Contains(4);
+//Console.WriteLine($"List contains 4? {status}");
+//status = myLinkedList.Contains(8);
+//Console.WriteLine($"List contains 8? {status}");
+
+//Console.WriteLine($"Sum: {myLinkedList.Sum()}");
+
+#endregion
+
+#region Recursive LinkedList
+var recursiveLinkedList = new RecursiveLinkedList();
+recursiveLinkedList.Append(1);
+recursiveLinkedList.Append(2);
+recursiveLinkedList.Append(3);
+recursiveLinkedList.Append(4);
+
+recursiveLinkedList.Print();
+
+var status = false;
+status = recursiveLinkedList.Contains(4);
+Console.WriteLine($"List contains 4? {status}");
+status = recursiveLinkedList.Contains(8);
+Console.WriteLine($"List contains 8? {status}");
+
+Console.WriteLine($"Sum: {recursiveLinkedList.Sum()}");
+
+#endregion
+
+#endregion
+
+
+Console.ReadLine();
