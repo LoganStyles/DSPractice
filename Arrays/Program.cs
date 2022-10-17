7ec9a81512420af21906efa06392930ea56a80eb
LoganStyles <emmaist23@gmail.com>
2022-10-17 11:14:26 +0100

    added an insert method for the custom array type

---
+++ Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+/**
+* Exercise to create an array class
+*/
+MyArray numbers = new MyArray(6);
+
+numbers.Insert(10);
+numbers.Insert(20);
+numbers.Insert(30);
+numbers.Insert(40);
+numbers.Insert(50);
+numbers.Insert(60);
+
+numbers.DisplayArray();
