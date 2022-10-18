bb26957baa354dd5e3969b81e4ded1fe3608624b
LoganStyles <emmaist23@gmail.com>
2022-10-18 14:06:46 +0100

    feat(arrays): added indexof & intersect methods

--- Arrays/MyArray.cs
+++ Arrays/MyArray.cs
@@ -14,6 +14,11 @@ public class MyArray
         return _count;
     }
 
+    /// inserts an item at a certain index.
+    ///
+    /// This operation expands the array when it is full.
+    /// @param item the item to be inserted
+    /// @note Big O time complexity => O(n)
     public void Insert(int item)
     {
 
@@ -35,7 +40,8 @@ public class MyArray
     /// removes an item at a certain index.
     ///
     /// This operation shrinks the array - hence we only process n-1 items to avoid IndexOutOfRangeException.
-    ///@param index the index to be removed
+    /// @param index the index to be removed
+    /// @note Big O time complexity => O(n)
     public void RemoveAt(int index)
     {
         if (index < 0 || index >= _count)
@@ -47,14 +53,115 @@ public class MyArray
             return;
         }
 
-        for (int i = index; i < _count-1; i++)
+        for (int i = index; i < _count - 1; i++)
         {
             _items[i] = _items[i + 1];
         }
 
         _count--;
     }
+    /// searches for an item
+    ///
+    /// @param item - the item to be searched
+    /// @note Big O time complexity => O(n)
+    public int IndexOf(int item)
+    {
+        for (int index = 0; index < _count; index++)
+        {
+            if (_items[index] == item)
+                return index;
+        }
 
+        return -1;
+    }
+
+    /// searches for the maximum item in an array.
+    ///
+    /// @note Big O time complexity => O(n)
+    public int Max()
+    {
+        if (_count == 0)
+            return -1;
+
+        int maxItem = _items[0];
+
+        for (int i = 0; i < _count; i++)
+        {
+            maxItem = (_items[i] > maxItem) ? _items[i] : maxItem;
+        }
+
+        return maxItem;
+    }
+
+    /// returns the common items between two arrays.
+    ///
+    /// .
+    /// @param array1 - the first array
+    /// @param array2 - the second array
+    /// @returns array with common items
+    /// @note Big O time complexity => O(n^2)
+    /// @note Big O space complexity => O(n)
+    public int[] Intersect(int[] array1, int[] array2)
+    {
+
+        int size = 0;
+
+        for (int i = 0; i < array1.Length; i++)
+        {
+            for (int j = 0; j < array2.Length; j++)
+            {
+                if (array1[i] == array2[j])
+                {
+                    size++;
+                }
+            }
+        }
+
+        int[] result = new int[size];
+        int countOfResult = 0;
+        for (int i = 0; i < array1.Length; i++)
+        {
+            for (int j = 0; j < array2.Length; j++)
+            {
+                if (array1[i] == array2[j])
+                {
+                    result[countOfResult++] = array1[i];
+                }
+            }
+        }
+
+        return result;
+
+    }
+
+
+    /// returns the common items between two arrays.
+    ///
+    /// This operation uses a list to temporarily hold the common items of both arrays.
+    /// @param array1 - the first array
+    /// @param array2 - the second array
+    /// @returns array with common items
+    /// @note Big O time complexity => O(n^2)
+    /// @note Big O space complexity => O(n)
+    public int[] IntersectUsingAList(int[] array1, int[] array2)
+    {
+
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < array1.Length; i++)
+        {
+            for (int j = 0; j < array2.Length; j++)
+            {
+                if (array1[i] == array2[j])
+                {
+                    result.Add(array1[i]);
+                }
+            }
+        }
+
+        return result.ToArray();
+
+    }
 
     public void DisplayArray()
     {
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -18,10 +18,33 @@ numbers.DisplayArray();
 #endregion
 
 #region Remove items
-numbers.RemoveAt(2);
-numbers.RemoveAt(4);
+// numbers.RemoveAt(2);
+// numbers.RemoveAt(4);
 // numbers.RemoveAt(10);//will throw an exception
-numbers.DisplayArray();
+// numbers.DisplayArray();
+#endregion
+
+#region Search
+// int searchItem = 70;
+// Console.WriteLine($"Index of {searchItem} is {numbers.IndexOf(searchItem)}");
+#endregion
+
+#region Find the maximum item in the array
+
+// Console.WriteLine(String.Format("The largest item is {0}", numbers.Max()));
+#endregion
+
+#region Find the common items in two arrays
+
+int[] array1 = { 10, 11, 11, 22, 30, 40 };
+int[] array2 = { 10, 16, 11, 12, 30, 50, 56 };
+
+var result = numbers.Intersect(array1, array2);
+Console.Write("The common items are - ");
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write($"{result[i]} ");
+}
 #endregion
 
 #endregion
